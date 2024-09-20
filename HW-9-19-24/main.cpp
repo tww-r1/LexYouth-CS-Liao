8d4ac288620355a20be28aefeed564250e8a1f9e
RyanJiang-py <thewaitwhatog@gmail.com>
2024-09-19 20:40:28 -0400

    9/19/24 hw

---
+++ HW-9-19-24/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+/*TODO Main: create a box with a plus sign in it: #######
+                                                  #  #  #
+                                                  #  #  #
+                                                  #######
+                                                  #  #  #
+                                                  #  #  #
+                                                  #######
+odd numbers should look similar
+Make it so even numbers can't be run on ( ie: end program if even number is entered)
+
+*/
+
+// TODO Voluntary: Write a function that reverses the array tony_hawk, then print it out
+int main()
+{
+    std::string tony_hawk[] = {"skateboarder.", "great", "a", "is", "Hawk", "Tony"}; // for TODO Voluntary
+}
