90c6c313bcddb6b37b2863d069d80d108edbb770
RyanJiang-py <thewaitwhatog@gmail.com>
2024-10-10 22:03:11 -0400

    Homework 10/10

---
+++ HW-10-10-24/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+// TODO 1: take in 5 intergers [note: cin splits 10 123 input as 10 then cin  again for 123] Use this formatting to output there lowest out of all 5 in '/' form
+// example:
+/*
+Enter 5 numbers: 1 2 3 4 5
+THE LARGEST ONE WAS /////
+*/
+
+/*
+TODO 2: [Note: add 1 to a counter each time an element is changed, compared, or accessed] Using the result from TODO 1, Fill a a vector from  0 to the result of TODO 1 backwards.
+Go through the vector and add 17 * (the value of the element) to said element.
+Use the sort() function to sort the vector [v is a placeholder for the vector sort(v.begin(), v.end()) POINTERS :) ]
+Now prompt the user for what number to find in the vector and rtell them whether not it is in the vector
+
+Enter a number to check membership of: 421
+That is not in the list! // or That is in the list!
+*/
+
+/*
+TODO 3 [yup, #2 is quite a lot]: print out the activity counter in the note section of TODO 2, and read this: https://www.geeksforgeeks.org/binary-search/
+ */
+
+int main()
+{
+
+    return 0;
+}
